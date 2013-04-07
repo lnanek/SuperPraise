bd40c09c80436650765636d92503948d4b180c8b
lance_nanek <lance_nanek@htc.com>
2013-04-06 20:01:49 -0700

    Full app runs on iPad.

--- SuperPraiseIos2/www/js/photos.js
+++ SuperPraiseIos2/www/js/photos.js
@@ -8,25 +8,17 @@ document.addEventListener("deviceready", onDeviceReady, false);
 // Cordova is ready to be used!
 //
 function onDeviceReady() {
-    console.log("onDeviceReady");
-    
 	pictureSource = navigator.camera.PictureSourceType;
 	destinationType = navigator.camera.DestinationType;
-    
-    capturePhoto();
 }
 
 // Called when a photo is successfully retrieved
 //
 function onPhotoDataSuccess(imageData) {
-    console.log("onPhotoDataSuccess");
-    
 	addImage("data:image/jpeg;base64," + imageData);
 }
 
 function addImage(src) {
-    console.log("addImage");
-    
 	var width = ($(window).width() - 40) / 2;
 	var insertion = '<img src="' + src + '" width="' + width + '"/>';	
 	$(".photoDestination").append(insertion);
@@ -36,30 +28,23 @@ function addImage(src) {
 // Called when a photo is successfully retrieved
 //
 function onPhotoURISuccess(imageURI) {
-    console.log("onPhotoURISuccess");
 	addImage(imageURI);
 }
 
 // A button will call this function
 //
 function capturePhoto() {
-    console.log("capturePhoto");
-    
 	// Take picture using device camera and retrieve image as base64-encoded
 	// string
 	navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
 		quality : 50,
-                                sourceType : navigator.camera.PictureSourceType.CAMERA,
-                                destinationType : navigator.camera.DestinationType.FILE_URI
-//                                destinationType : destinationType.DATA_URL
+		destinationType : destinationType.DATA_URL
 	});
 }
 
 // A button will call this function
 //
 function capturePhotoEdit() {
-    console.log("capturePhotoEdit");
-    
 	// Take picture using device camera, allow edit, and retrieve image as
 	// base64-encoded string
 	navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
